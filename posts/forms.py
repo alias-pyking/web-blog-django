7c866407716460758cd2ebfadb4a8e8d8a6da5c0
shubhamsks <shubhamkumar7051@gmail.com>
2019-04-06 00:37:05 +0530

    Removed PageDown Widget

--- posts/forms.py
+++ posts/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 from .models import Post
-from pagedown.widgets import PagedownWidget
-
 
 class PostForm(forms.ModelForm):
-    content = forms.CharField(widget=PagedownWidget(show_preview=False))
-
+    content = forms.CharField(widget=forms.Textarea)
     class Meta:
         model = Post
         fields = ['title', 'content', 'image']
