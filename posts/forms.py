e31563fa2b6c6aa44c3951f3b3e078f827a73eee
shubhamsks <shubhamkumar7051@gmail.com>
2019-04-10 00:21:40 +0530

    Removed Pagedown

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from pagedown.widgets import PagedownWidget
 
 class PostForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea)
